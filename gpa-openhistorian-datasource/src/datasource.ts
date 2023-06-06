d585f15ad40cf4d914607a0a6f17b62a13f63ea6
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-06-06 16:21:18 -0400

    Fixed indentation + error with duplicate entrees

--- gpa-openhistorian-datasource/src/datasource.ts
+++ gpa-openhistorian-datasource/src/datasource.ts
@@ -6,12 +6,12 @@ import {
   MutableDataFrame,
   FieldType,
   QueryResultMeta,
-} from '@grafana/data';
+} from "@grafana/data";
 
-import { MyQuery, MyDataSourceOptions } from './types';
-import { getBackendSrv } from '@grafana/runtime';
-import _ from 'lodash';
-import { DefaultFlags } from './js/constants'
+import { MyQuery, MyDataSourceOptions } from "./types";
+import { getBackendSrv } from "@grafana/runtime";
+import _ from "lodash";
+import { DefaultFlags } from "./js/constants";
 
 interface FieldMeta {
   [key: string]: number | undefined;
@@ -31,7 +31,6 @@ interface MetadataTarget {
   excludeNormalFlags: boolean;
 }
 
-
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   url: string;
   flags: {
@@ -41,66 +40,54 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     [key: string]: boolean;
   };
 
-  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+  constructor(
+    instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>
+  ) {
     super(instanceSettings);
 
     this.url = instanceSettings.jsonData.http.url || "";
     this.flags = instanceSettings.jsonData.flags || {};
     this.metadata = instanceSettings.jsonData.metadata || {};
-     
   }
 
   //List of all elements
-  async searchQuery(){
+  async searchQuery() {
     return await getBackendSrv().datasourceRequest({
       url: this.url + "/search",
-      method: 'POST',
-      data: { target: "" }
+      method: "POST",
+      data: { target: "" },
     });
   }
 
   //Information on specific elements
-  async dataQuery(query: DataQueryRequest<MyQuery>){
+  async dataQuery(query: DataQueryRequest<MyQuery>) {
     return await getBackendSrv().datasourceRequest({
-      url: this.url + '/query',
+      url: this.url + "/query",
       data: query,
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' }
-    })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
   }
 
   //Metadata of specific elements
-  async metadataQuery(target: MetadataTarget){
+  async metadataQuery(target: MetadataTarget) {
     return await getBackendSrv().datasourceRequest({
-      url: this.url + '/getmetadata',
+      url: this.url + "/getmetadata",
       data: target,
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' }
-    })
-  }
-
-  // Metadata of specific elements
-  async metadatasQuery(targets: MetadataTarget[]){
-    return await getBackendSrv().datasourceRequest({
-      url: this.url + '/getmetadatas',
-      data: targets,
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' }
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
     });
   }
 
-
-
-  //Information on specific elements
-  // async metadataOptionsQuery() {
-  // const response = await getBackendSrv().datasourceRequest({
-  //   url: this.url + '/getmetadataoptions',
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/json' }
-  // });
-  // return response.data;
-  // }
-
+  // Metadata of specific elements
+  async metadatasQuery(targets: MetadataTarget[]) {
+    return await getBackendSrv().datasourceRequest({
+      url: this.url + "/getmetadatas",
+      data: targets,
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
   //Generate value of excluded flags
   calculateFlags() {
@@ -116,67 +103,72 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
   fixTemplates(target: MyQuery) {
-    if (target === undefined){
-      return '';
-    } 
+    if (target === undefined || target.elements === undefined) {
+      return "";
+    }
 
-    let sep = ' ';
-    if(!target.queryType || target.queryType === 'Element List'){
-      sep = ';'
+    let sep = " ";
+    if (!target.queryType || target.queryType === "Element List") {
+      sep = ";";
     }
 
     return target.elements.join(sep);
   }
 
-  buildQueryParameters(options: DataQueryRequest<MyQuery>) {
-    let _this = this; 
-    const excludedFlags = _this.calculateFlags()
-    const excludeNormalFlags = _this.flags["Normal"] ? _this.flags["Normal"] : false
 
-    let targets = _.map(options.targets, function (target) {
-      return {
-        target: _this.fixTemplates(target),
-        refId: target.refId,
-        hide: target.hide, 
+  buildQueryParameters(options: DataQueryRequest<MyQuery>) {
+    const excludedFlags = this.calculateFlags();
+    const excludeNormalFlags = this.flags["Normal"]
+      ? this.flags["Normal"]
+      : false;
+
+    const targets = options.targets.map((target) => ({
+      target: this.fixTemplates(target),
+      refId: target.refId,
+      hide: target.hide,
+      excludedFlags: excludedFlags,
+      excludeNormalFlags: excludeNormalFlags,
+      queryOptions: {
         excludedFlags: excludedFlags,
         excludeNormalFlags: excludeNormalFlags,
-        queryOptions: {
-          excludedFlags: excludedFlags,
-          excludeNormalFlags:excludeNormalFlags,
-        },
-        queryType: target.queryType ? target.queryType : "Element List",
-        elements: target.elements,
-      };
-    });
-    
+      },
+      queryType: target.queryType ? target.queryType : "Element List",
+      elements: target.elements,
+    }));
 
     options.targets = targets;
 
     return options;
   }
 
-  buildMetadataParameters(options: DataQueryRequest<MyQuery>): MetadataTarget[] {
-    let _this = this; 
-    const excludedFlags = _this.calculateFlags()
-    const excludeNormalFlags = _this.flags["Normal"] ? _this.flags["Normal"] : false
-  
+  buildMetadataParameters(
+    options: DataQueryRequest<MyQuery>
+  ): MetadataTarget[] {
+    const excludedFlags = this.calculateFlags();
+    const excludeNormalFlags = this.flags["Normal"]
+      ? this.flags["Normal"]
+      : false;
+
     const targets: MetadataTarget[] = [];
-  
-    options.targets.forEach(function (target) {
-      for(const element of target.elements){
+
+    for (const target of options.targets) {
+      if (!target.elements) {
+        continue;
+      }
+
+      for (const element of target.elements) {
         targets.push({
           refId: target.refId,
           target: element,
-          type: "table", 
+          type: "table",
           excludedFlags: excludedFlags,
-          excludeNormalFlags: excludeNormalFlags
+          excludeNormalFlags: excludeNormalFlags,
         });
       }
-    });
-  
+    }
+
     return targets;
   }
-
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
     const { range } = options;
@@ -185,128 +177,128 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
     //Filter metadata options to include only those that are selected (true)
     const selectedMetadataOptions = Object.keys(this.metadata).filter(
-      key => this.metadata[key] === true && key !== "Select All"
+      (key) => this.metadata[key] === true && key !== "Select All"
     );
 
-    //Return a constant for each query.
-    const dataPromises = options.targets.map(async (target) => { 
-      if(!target.elements){
-        return new MutableDataFrame({
-          refId: target.refId,
-          fields: [
-            { name: 'Time', values: [from, to], type: FieldType.time },
-          ],
-        });
-      }
+    // Use only the first target. You will need to decide how to select this based on your application logic.
+    const target = options.targets[0];
 
-      //Undefined or element
-      if(!target.queryType || target.queryType === 'Element List'){
-        let query = this.buildQueryParameters(options);
+    if (!target.elements) {
+      return {
+        data: [
+          new MutableDataFrame({
+            refId: target.refId,
+            fields: [
+              { name: "Time", values: [from, to], type: FieldType.time },
+            ],
+          }),
+        ],
+      };
+    }
 
-        //Remove all hidden elements
-        query.targets = query.targets.filter(function (t) {
-          return !t.hide;
-        });
+    // Undefined or element
+    if (!target.queryType || target.queryType === "Element List") {
+      let query = this.buildQueryParameters(options);
 
-        //Get Data
-        const pointsData = await this.dataQuery(query)
+      // Remove all hidden elements
+      query.targets = query.targets.filter(function (t) {
+        return !t.hide;
+      });
 
-        let metadataParameters = this.buildMetadataParameters(options);
-        const metadataResponse = await this.metadatasQuery(metadataParameters)
-        let metadataParsed = JSON.parse(metadataResponse.data);
+      // Get Data
+      const pointsData = await this.dataQuery(query);
 
-        
-        //Declare frames
-        const frame = new MutableDataFrame({
-          refId: target.refId,
-          fields: [
-            { name: 'Time', type: FieldType.time },
-          ],
-          meta: {
-            fields: {},
-          } as CustomQueryResultMeta, // Use the custom interface
-        });
+      let metadataParameters = this.buildMetadataParameters(options);
+      const metadataResponse = await this.metadatasQuery(metadataParameters);
+      let metadataParsed = JSON.parse(metadataResponse.data);
 
-        const metadata = frame.meta as CustomQueryResultMeta;
-        const fieldMetadata = metadata.fields;
+      // Declare frames
+      const frame = new MutableDataFrame({
+        refId: target.refId,
+        fields: [{ name: "Time", type: FieldType.time }],
+        meta: {
+          fields: {},
+        } as CustomQueryResultMeta, // Use the custom interface
+      });
 
-        //Add fields & meta data
-        for (const entry of pointsData["data"]) {
-          frame.addField({ name: entry["target"], type: FieldType.number })
-          //const entryMeta = entry["meta"]["custom"];
-    
-          //Initialize metadata for current field
-          fieldMetadata[entry["target"]] = {};
+      const metadata = frame.meta as CustomQueryResultMeta;
+      const fieldMetadata = metadata.fields;
 
-          //Populate selected metadata options
-          for (const metadataOption of selectedMetadataOptions) {
-            fieldMetadata[entry["target"]][metadataOption] = metadataParsed[entry["target"]][0][metadataOption]
-          }
+      // Add fields & meta data
+      for (const entry of pointsData["data"]) {
+        frame.addField({ name: entry["target"], type: FieldType.number });
+
+        // Initialize metadata for current field
+        fieldMetadata[entry["target"]] = {};
+
+        // Populate selected metadata options
+        for (const metadataOption of selectedMetadataOptions) {
+          fieldMetadata[entry["target"]][metadataOption] =
+            metadataParsed[entry["target"]][0][metadataOption];
         }
-        
-        console.log("Meta Data")
-        console.log(metadata)
-
-        //Intermediate object to group points by timestamp
-        const groupedPoints: { [timestamp: number]: { [target: string]: number; }; } = groupPoints(pointsData);
-
-        //Iterate through grouped points and add them to the frame
-        for (const timestamp in groupedPoints) {
-          const data: { [key: string]: any } = { Time: parseInt(timestamp, 10) };
-          Object.assign(data, groupedPoints[timestamp]);
-          frame.add(data);
-        }
-
-        return frame
       }
-      // else if(target.queryType === 'filter'){
 
-      // }
-      // else if(target.queryType === 'text'){
+      // Intermediate object to group points by timestamp
+      const groupedPoints: {
+        [timestamp: number]: { [target: string]: number };
+      } = groupPoints(pointsData);
 
-      // }
-      else{
-        // return new MutableDataFrame({
-        //   refId: target.refId,
-        //   fields: [
-        //     { name: 'Time', values: [from, to], type: FieldType.time },
-        //     //{ name: 'Value', values: [target.constant, target.constant], type: FieldType.number },
-        //   ],
-        // });
+      // Iterate through grouped points and add them to the frame
+      for (const timestamp in groupedPoints) {
+        const data: { [key: string]: any } = { Time: parseInt(timestamp, 10) };
+        Object.assign(data, groupedPoints[timestamp]);
+        frame.add(data);
       }
-      return new MutableDataFrame({
-          refId: target.refId,
-          fields: [
-            { name: 'Time', values: [from, to], type: FieldType.time },
-          ],
-        });
-    });
 
-    const data = await Promise.all(dataPromises);
+      return { data: [frame] };
+    }
 
-
-    return { data };
+    // else if(target.queryType === 'filter'){
+    // }
+    // else if(target.queryType === 'text'){
+    // }
+    else {
+      return {
+        data: [
+          new MutableDataFrame({
+            refId: target.refId,
+            fields: [
+              { name: "Time", values: [from, to], type: FieldType.time },
+            ],
+          }),
+        ],
+      };
+    }
   }
 
   async testDatasource() {
-    return await getBackendSrv().datasourceRequest({
-        url: this.url + '/',
-        method: 'GET'
-      }).then(function (response) {
+    return await getBackendSrv()
+      .datasourceRequest({
+        url: this.url + "/",
+        method: "GET",
+      })
+      .then(function (response) {
         if (response.status === 200) {
-          return { status: "success", message: "Data source is working", title: "Success" };
+          return {
+            status: "success",
+            message: "Data source is working",
+            title: "Success",
+          };
+        } else {
+          return {
+            status: "error",
+            message: "Data source is not working",
+            title: "Error",
+          };
         }
-        else {
-          return { status: "error", message: "Data source is not working", title: "Error" };
-        }
-      }
-    );
+      });
   }
 }
 
 //Groups data by time
 function groupPoints(pointsData: { [key: string]: any }) {
-  const groupedPoints: { [timestamp: number]: { [target: string]: number; }; } = {};
+  const groupedPoints: { [timestamp: number]: { [target: string]: number } } =
+    {};
 
   //Iterate through each entry
   for (const entry of pointsData["data"]) {
@@ -323,4 +315,3 @@ function groupPoints(pointsData: { [key: string]: any }) {
   }
   return groupedPoints;
 }
-
